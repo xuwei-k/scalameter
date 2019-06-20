10ac2ea34bc030f5db1c03a8fc53657ac66b3509
xuwei-k <6b656e6a69@gmail.com>
2019-06-20 12:17:27 +0900

    Scala 2.13.0

--- project/Build.scala
+++ project/Build.scala
@@ -58,9 +58,20 @@ object ScalaMeterBuild extends MechaRepoBuild {
     name := "scalameter",
     organization := "com.storm-enroute",
     scalaVersion := "2.11.12",
-    crossScalaVersions := Seq("2.11.12", "2.12.8", "2.13.0-M3"),
+    crossScalaVersions := Seq("2.11.12", "2.12.8", "2.13.0"),
     scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature", "-Xlint", "-Xfuture"),
-    libraryDependencies ++= dependencies(scalaVersion.value),
+    libraryDependencies ++= Seq(
+      "org.scalatest" %% "scalatest" % "3.0.8" % "test",
+      "junit" % "junit" % "4.12" % "test",
+      "org.apache.commons" % "commons-math3" % "3.2",
+      "org.scala-sbt" % "test-interface" % "1.0",
+      "org.scala-lang.modules" %% "scala-xml" % "1.2.0",
+      "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2",
+      "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.9.9",
+      "commons-io" % "commons-io" % "2.4",
+      "io.spray" %% "spray-json" % "1.3.5",
+      "org.jline" % "jline" % "3.10.0"
+    ),
     parallelExecution in Test := false,
     fork := true,
     fork in run := true,
@@ -104,59 +115,14 @@ object ScalaMeterBuild extends MechaRepoBuild {
         </developer>
       </developers>
   )
-
-  def dependencies(scalaVersion: String) = {
-    CrossVersion.partialVersion(scalaVersion) match {
-      case Some((2, 13)) => List(
-        "org.scalatest" % "scalatest_2.13.0-M2" % "3.0.4" % "test",
-        "junit" % "junit" % "4.12" % "test",
-        "org.apache.commons" % "commons-math3" % "3.2",
-        "org.scala-sbt" % "test-interface" % "1.0",
-        "org.scala-lang.modules" % "scala-xml_2.13.0-M2" % "1.0.6",
-        "org.scala-lang.modules" % "scala-parser-combinators_2.13.0-M2" % "1.0.7",
-        "com.fasterxml.jackson.module" % "jackson-module-scala_2.12" % "2.9.9",
-        "org.mongodb.scala" % "mongo-scala-driver_2.12" % "2.2.0",
-        "commons-io" % "commons-io" % "2.4",
-        "io.spray" % "spray-json_2.13.0-M2" % "1.3.4",
-        "org.jline" % "jline" % "3.10.0"
-      )
-      case Some((2, 12)) => List(
-        "org.scalatest" % "scalatest_2.12" % "3.0.0" % "test",
-        "junit" % "junit" % "4.12" % "test",
-        "org.apache.commons" % "commons-math3" % "3.2",
-        "org.scala-sbt" % "test-interface" % "1.0",
-        "org.scala-lang.modules" % "scala-xml_2.12" % "1.0.5",
-        "org.scala-lang.modules" % "scala-parser-combinators_2.12" % "1.0.4",
-        "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.9.9",
-        "org.mongodb.scala" %% "mongo-scala-driver" % "2.2.0",
-        "commons-io" % "commons-io" % "2.4",
-        "io.spray" % "spray-json_2.12" % "1.3.2",
-        "org.jline" % "jline" % "3.10.0"
-      )
-      case Some((2, 11)) => List(
-        "org.scalatest" %% "scalatest" % "3.0.0" % "test",
-        "junit" % "junit" % "4.12" % "test",
-        "org.apache.commons" % "commons-math3" % "3.2",
-        "org.scala-sbt" % "test-interface" % "1.0",
-        "org.scala-lang.modules" %% "scala-xml" % "1.0.1",
-        "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.1",
-        "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.9.9",
-        "org.mongodb.scala" %% "mongo-scala-driver" % "2.2.0",
-        "commons-io" % "commons-io" % "2.4",
-        "io.spray" %% "spray-json" % "1.3.2",
-        "org.jline" % "jline" % "3.10.0"
-      )
-      case _ => Nil
-    }
-  }
 
   val scalaMeterCoreSettings = MechaRepoPlugin.defaultSettings ++ publishCreds ++ Seq(
     name := "scalameter-core",
     organization := "com.storm-enroute",
     scalaVersion := "2.11.12",
-    crossScalaVersions := Seq("2.11.12", "2.12.8", "2.13.0-M3"),
-    scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature", "-Xlint", "-Xfuture"),
-    libraryDependencies ++= coreDependencies(scalaVersion.value),
+    crossScalaVersions := Seq("2.11.12", "2.12.8", "2.13.0"),
+    scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature", "-Xlint"),
+    libraryDependencies ++= coreDependencies,
     parallelExecution in Test := false,
     fork := true,
     fork in run := true,
@@ -201,45 +167,18 @@ object ScalaMeterBuild extends MechaRepoBuild {
       </developers>
   )
 
-  def coreDependencies(scalaVersion: String) = CrossVersion.partialVersion(scalaVersion) match {
-    case Some((2, 13)) => List(
-      "org.scalacheck" % "scalacheck_2.13.0-M2" % "1.13.5" % "test",
-      "org.scalatest" % "scalatest_2.13.0-M2" % "3.0.4" % "test",
-      "junit" % "junit" % "4.12" % "test",
-      "com.novocode" % "junit-interface" % "0.11" % "test",
-      "org.apache.commons" % "commons-math3" % "3.2",
-      "org.apache.commons" % "commons-lang3" % "3.4",
-      "org.scala-lang.modules" % "scala-xml_2.13.0-M2" % "1.0.6",
-      "org.scala-lang.modules" % "scala-parser-combinators_2.13.0-M2" % "1.0.7",
-      "org.ow2.asm" % "asm" % "5.0.4",
-      "org.jline" % "jline" % "3.10.0"
-    )
-    case Some((2, 12)) => List(
-      "org.scalacheck" % "scalacheck_2.12" % "1.13.4" % "test",
-      "org.scalatest" % "scalatest_2.12" % "3.0.0" % "test",
-      "junit" % "junit" % "4.12" % "test",
-      "com.novocode" % "junit-interface" % "0.11" % "test",
-      "org.apache.commons" % "commons-math3" % "3.2",
-      "org.apache.commons" % "commons-lang3" % "3.4",
-      "org.scala-lang.modules" % "scala-xml_2.12" % "1.0.5",
-      "org.scala-lang.modules" % "scala-parser-combinators_2.12" % "1.0.4",
-      "org.ow2.asm" % "asm" % "5.0.4",
-      "org.jline" % "jline" % "3.10.0"
-    )
-    case Some((2, 11)) => List(
-      "org.scalacheck" %% "scalacheck" % "1.12.4" % "test",
-      "org.scalatest" %% "scalatest" % "2.2.6" % "test",
-      "junit" % "junit" % "4.12" % "test",
-      "com.novocode" % "junit-interface" % "0.11" % "test",
-      "org.apache.commons" % "commons-math3" % "3.2",
-      "org.apache.commons" % "commons-lang3" % "3.4",
-      "org.scala-lang.modules" %% "scala-xml" % "1.0.1",
-      "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.1",
-      "org.ow2.asm" % "asm" % "5.0.4",
-      "org.jline" % "jline" % "3.10.0"
-    )
-    case _ => Nil
-  }
+  val coreDependencies = Seq(
+    "org.scalacheck" %% "scalacheck" % "1.14.0" % "test",
+    "org.scalatest" %% "scalatest" % "3.0.8" % "test",
+    "junit" % "junit" % "4.12" % "test",
+    "com.novocode" % "junit-interface" % "0.11" % "test",
+    "org.apache.commons" % "commons-math3" % "3.2",
+    "org.apache.commons" % "commons-lang3" % "3.4",
+    "org.scala-lang.modules" %% "scala-xml" % "1.2.0",
+    "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2",
+    "org.ow2.asm" % "asm" % "5.0.4",
+    "org.jline" % "jline" % "3.10.0"
+  )
 
   val javaCommand = TaskKey[String](
     "java-command",
